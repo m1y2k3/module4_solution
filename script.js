d0e9eaee49cba5632a6f69891e87ba0c0660c119
m1y2k3 <68805856+m1y2k3@users.noreply.github.com>
2021-01-04 10:53:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function () {
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Mayank", "Rishabh", "Bhola", "Pakhi", "Lala", "Jack"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jack"];
 
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
